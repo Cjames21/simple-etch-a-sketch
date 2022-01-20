d71083280a957b35a353da120b82cc0fdac874f5
Charles James <charlestdjames1@gmail.com>
2022-01-20 10:27:19 -0500

    Project finished. Extra styling is all that's left.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // Global variables
-var gameActive = false;
-var gridSize = 40;
+const DEFAULT_COLOR = "#000000";
+const DEFAULT_MODE = "black";
+const COLOR_ARR = ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000"];
+const GAME_MODES = ["black", "rainbow", "chosenColor", "eraser"];
 
+var gameActive = false;
+var gameMode = DEFAULT_MODE;
+var gridSize = 40;
+var chosenColor = DEFAULT_COLOR;
+
+
+var colorsContainer;
+
+
+// Onload initialization
+window.onload = () => {
+    createGrid();
+
+    addColorArr();    
+}
 
 // General functionality
 
@@ -15,27 +32,54 @@ function createGrid() {
 
     */ 
     var gameArea = document.getElementById("gameArea");
-
     
 
     for(let i = 0; i < gridSize * gridSize; i++) {
         var element = document.createElement("div");
-        element.setAttribute("class", "flex-item--panel");
-        // element.setAttribute("id", "panel-" + i);
-        element.style.display = "inline-block";
-        element.style.width = "calc(100%/" + gridSize + ")";
-        element.style.paddingTop = "calc(100%/" + gridSize + ")";
-        element.style.backgroundColor = "#ff9fcf";
-        element.style.margin = "0";
-        element.addEventListener("mouseenter", function(e) {
-            if(gameActive) {
-                e.target.style.backgroundColor = "black";
-            }
+            element.setAttribute("class", "flex-item--panel");
+            element.style.display = "inline-block";
+            element.style.width = "calc(100%/" + gridSize + ")";
+            element.style.paddingTop = "calc(100%/" + gridSize + ")";
+            element.style.backgroundColor = "#b2dfdb";
+            element.style.margin = "0";
+            element.addEventListener("mouseenter", function(e) {
+                if(gameActive) {
+                    if(gameMode === DEFAULT_MODE) {
+                        e.target.style.backgroundColor = DEFAULT_COLOR;
+                    } else if(gameMode === "rainbow") {
+                        var rand = Math.floor(Math.random() * 7);
+                        e.target.style.backgroundColor = COLOR_ARR[rand];
+                    } else if(gameMode === GAME_MODES[2]){
+                        e.target.style.backgroundColor = chosenColor;
+                    } else if(gameMode === GAME_MODES[3]) {
+                        e.target.style.backgroundColor = "#b2dfdb";
+                    }
+                }
         })
         gameArea.append(element);
     }
 
     gameActive = true;
+}
+
+function addColorArr() {
+    var colorsContainer = document.getElementById("colorsCont");
+
+    for(let i = 0; i < COLOR_ARR.length; i++) {
+        var colorElement = document.createElement("button");
+            colorElement.setAttribute("class", "btn-color");
+            colorElement.style.display = "inline-block";
+            colorElement.style.width = "calc(95%/7)";
+            colorElement.style.paddingTop = "calc(90%/7)";
+            colorElement.style.border = "1px solid grey";
+            colorElement.style.backgroundColor = COLOR_ARR[i];
+            colorElement.addEventListener("click", function(e){
+                gameMode = GAME_MODES[2];
+                chosenColor = COLOR_ARR[i];
+            })
+
+        colorsContainer.append(colorElement);
+    }
 }
 
 function resetGameHTML() {
@@ -47,11 +91,20 @@ function toggleGameActive() {
     gameActive = !gameActive;
 }
 
+function createElement() {
+    var element = document.createElement("div");
+        element.setAttribute("class", "flex-item--panel");
+        // element.setAttribute("id", "panel-" + i);
+        element.style.display = "inline-block";
+        element.style.width = "calc(100%/" + gridSize + ")";
+        element.style.paddingTop = "calc(100%/" + gridSize + ")";
+        element.style.backgroundColor = "#ff9fcf";
+        element.style.margin = "0";
+
+        return element;
+}
+
 // Event listeners
-var enableBtn = document.getElementById("enableGame");
-enableBtn.addEventListener("click", function() {
-    createGrid();
-})
 
 var disableBtn = document.getElementById("disableGame");
 disableBtn.addEventListener("click", function() {
@@ -60,6 +113,7 @@ disableBtn.addEventListener("click", function() {
 
 var sizeInput = document.getElementById("changeSize");
 sizeInput.addEventListener("click", function() {
+    resetGameHTML();
     let sizeView = document.getElementById("sizeView");
     let userInput = parseInt(document.getElementById("uInput").value);
     console.log(userInput);
@@ -68,6 +122,21 @@ sizeInput.addEventListener("click", function() {
     
     createGrid();
  
+})
+
+var btnRainbow = document.getElementById("rainbow-pen");
+btnRainbow.addEventListener("click", () => {
+    gameMode = GAME_MODES[1];
+})
+
+var btnBlack = document.getElementById("black-pen");
+btnBlack.addEventListener("click", () => {
+    gameMode = GAME_MODES[0];
+})
+
+var btnEraser = document.getElementById("eraser");
+btnEraser.addEventListener("click", () => {
+    gameMode = GAME_MODES[3];
 })
 
 document.addEventListener("keydown", function(e) {
